88fbe1027441abcde0c03bb898762f9fd8ae0d15
ZairAlejandroBulos <zair.bulos@alumnos.frm.utn.edu.ar>
2023-05-08 16:17:30 -0300

    Add TestApiCall Auth0

--- src/components/Auth0/AuthProvider.tsx
+++ src/components/Auth0/AuthProvider.tsx
@@ -8,12 +8,14 @@ interface Props {
 function AuthProvider({ children }: Props): JSX.Element {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   return (
     <Auth0Provider
       domain={domain as string}
       clientId={clientId as string}
       authorizationParams={{
+        audience: audience as string,
         redirect_uri: window.location.origin,
       }}
     >
