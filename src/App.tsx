2112f8736d06c0eb4309bf8f79e43506e47ba1be
ZairAlejandroBulos <zair.bulos@alumnos.frm.utn.edu.ar>
2023-05-24 11:12:21 -0300

    Config project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Footer from "./components/Footer/Footer";
+import Footer from "./components/Layout/Footer/Footer";
 import IndexRouter from "./routers/IndexRouter";
 
 function App() {
