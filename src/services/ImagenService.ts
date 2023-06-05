ff40381bf42a19c0635d7c695c17b9f068ef3add
ZairAlejandroBulos <zair.bulos@alumnos.frm.utn.edu.ar>
2023-06-05 15:38:44 -0300

    Add save Imagen

--- src/services/ImagenService.ts
+++ src/services/ImagenService.ts
@@ -24,3 +24,31 @@ export async function findImagenByName(nombre: string, token: string) {
         throw new Error(`Error! ${error}`);
     }
 }
+
+export async function saveImagen(file: File, nombre: string, token: string) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+        const response = await fetch(`${URL_API_BASE}/imagenes/${nombre}`, {
+            method: "POST",
+            body: formData,
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "enctype": "multipart/form-data"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.blob();
+        const imagenUrl = URL.createObjectURL(data);
+
+        return imagenUrl;
+    } catch (error) {
+        console.log(error);
+        throw new Error(`Error! ${error}`);
+    }
+}
